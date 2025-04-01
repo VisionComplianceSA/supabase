91e0ca4dc456f9927d8905293ac76aa3828385ac
Maxime Rossier <rossier.maxime@gmail.com>
2025-04-01 16:27:21 +0200

    add auth

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,38 @@ async function getUsers() {
   const { data } = await supabase.from('users').select()
   users.value = data
 }
+
+const email = 'dev@visioncompliance.ch'
+const otp = ref()
+async function signin() {
+  const { data, error } = await supabase.auth.signInWithOtp({
+    email,
+  })
+  console.log(data, error)
+}
+async function verifyOTP() {
+  const { data, error } = await supabase.auth.verifyOtp({
+    email,
+    token: otp.value,
+    type: 'email',
+  })
+  console.log(data, error)
+}
+
 onMounted(() => {
   getUsers()
+  supabase.auth.getUser().then((user) => {
+    console.log('getUser', user)
+  })
 })
 </script>
 
 <template>
   <ul>
     <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    <button @click="signin">signin</button>
+    otp
+    <input type="text" v-model="otp" />
+    <button @click="verifyOTP">verify</button>
   </ul>
 </template>
